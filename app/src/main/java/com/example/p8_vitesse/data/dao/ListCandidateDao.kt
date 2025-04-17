03b6acf63c916a51847c1d5ec216e41556189023
JM97142 <jeanmichel.taret@gmail.com>
2025-04-17 06:06:24 +0200

    add dao

--- app/src/main/java/com/example/p8_vitesse/data/dao/ListCandidateDao.kt
+++ app/src/main/java/com/example/p8_vitesse/data/dao/ListCandidateDao.kt
@@ -1,4 +1,16 @@
 package com.example.p8_vitesse.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.p8_vitesse.data.entity.listCandidateDto
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface ListCandidateDao {
+    @Insert
+    suspend fun insertCandidate(listCandidateDto: listCandidateDto): Long
+
+    @Query("SELECT * from list_candidate")
+    fun getAllCanddidates(): Flow<List<listCandidateDto>>
 }
